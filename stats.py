bd4cf1f19707b846924e207b446f89f24aa9e91d
Ludwig <hoffmann@lorentz.leidenuniv.nl>
2021-01-25 16:41:47 +0100

    Bar chart added and word count improved

--- stats.py
+++ stats.py
@@ -1,12 +1,14 @@
 import re
-from collections import Counter
+import collections
+import matplotlib.pyplot as plt
+
 
 #file = open("test.txt", "r")
 file = open("war_and_peace.txt", "r")
 
 data = file.read()
-data_wout_symbols = re.sub(r'[^\w]', ' ', data)
-words = data_wout_symbols.split()
+data_wout_symbols = re.sub(r'[^\w]', ' ', data) #remove symbols
+words = data_wout_symbols.split() #split text at space
 
 for i in range(len(words)):
     #make all words lowercase
@@ -14,7 +16,12 @@ for i in range(len(words)):
 
 #print(words)
 print('Number of words:', len(words))
-frequency_letter = Counter(data_wout_symbols)
-frequency_words = Counter(words)
-#print(frequency_letter)
-print(frequency_words.most_common(5))
+frequency_letter = collections.Counter(data_wout_symbols) #count how often each letter occurs
+frequency_words = collections.Counter(words) #count how often each word occurs
+#print(frequency_letter) #print frequency of letters
+print(frequency_words.most_common(5)) #print x most common words
+print(frequency_words['war'],frequency_words['peace']) #print frequency of given word
+frequency_words_decreasing = collections.OrderedDict(frequency_words.most_common(1000)) #Most common returns list and we convert it back into an ordered dictionary for the barchart to work
+plt.bar(frequency_words_decreasing.keys(), frequency_words_decreasing.values())
+plt.savefig('bardiagram.png')
+
