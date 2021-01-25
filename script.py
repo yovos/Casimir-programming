5511671d94251458ba3e354b06f55420e61f961a
yoramvos <Y.Vos-1@tudelft.nl>
2021-01-25 14:59:22 +0100

    Saves plot to a .png

--- script.py
+++ script.py
@@ -11,5 +11,8 @@ draw_circle = plt.Circle((0.5, 0.5), 0.3)
 
 axes.set_aspect(1)
 axes.add_artist(draw_circle)
+
 plt.title('Circle')
+
+plt.savefig('plotted_circle.png', bbox_inches='tight')
 plt.show()
