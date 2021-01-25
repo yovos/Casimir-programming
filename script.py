66355f31b0529b057a1d53659d1bf18bb732c39b
Ludwig <hoffmann@lorentz.leidenuniv.nl>
2021-01-25 14:56:30 +0100

    Added plot circle

--- script.py
+++ script.py
@@ -1,5 +1,15 @@
 import test
+import matplotlib.pyplot as plt
 
 print('Hello world')
 print('The circumference of a circule with radius 3 is: {}'.format(test.circumference(3)))
 print('The area of a circle with radius 3 is: {}'.format(test.area(3))) 
+
+
+figure, axes = plt.subplots()
+draw_circle = plt.Circle((0.5, 0.5), 0.3)
+
+axes.set_aspect(1)
+axes.add_artist(draw_circle)
+plt.title('Circle')
+plt.show()
