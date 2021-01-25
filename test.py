57e4bc1d37b41113652dd438a3eb24c32d33d667
Ludwig <hoffmann@lorentz.leidenuniv.nl>
2021-01-25 14:57:57 +0100

    Fixed Indention

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import math
 
 def circumference(r):
     """Calculate the surface of a circle of radius r
-	This does not compute the surface though :D"""
-	return 2*math.pi*r
+    This does not compute the surface though :D"""
+    return 2*math.pi*r
 
 def area(r):
     """
     Function calculating area of circle of radius r
     """
-	return math.pi*r**2
+    return math.pi*r**2
 
 print("Hello world")
