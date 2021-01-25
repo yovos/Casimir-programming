ddeb7a794908833dfd223163fc66170730088328
yoramvos <Y.Vos-1@tudelft.nl>
2021-01-25 14:44:58 +0100

    Added a nonsense comment to circumference function

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import math
 
 def circumference(r):
-    """Calculate the surface of a circle of radius r"""
+    """Calculate the surface of a circle of radius r
+    
+    I swear this time we'll break you git, it's going to hurt.
+    """
 	return 2*math.pi*r
 
 def area(r):
