29846887f85918bbd40b9bf1c57171d0923c6934
yoramvos <Y.Vos-1@tudelft.nl>
2021-01-25 15:04:46 +0100

    Added function to calculate the area of a triangle

--- test.py
+++ test.py
@@ -11,4 +11,14 @@ def area(r):
     """
     return math.pi*r**2
 
+def area_triangle(base, height):
+    """
+    Calculate the area of a triangle.
+    
+    param "base": width of the triangle base
+    param "height": height of the trianble
+    """
+    return 0.5*base*height
+    
+
 print("Hello world")
