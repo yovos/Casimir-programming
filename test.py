3ac96f267620f163a58fd76b3fb03969b7554ebc
Ludwig <hoffmann@lorentz.leidenuniv.nl>
2021-01-25 14:44:18 +0100

    Added comment circ function

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import math
 
 def circumference(r):
-    """Calculate the surface of a circle of radius r"""
+    """Calculate the surface of a circle of radius r
+	This does not compute the surface though :D"""
 	return 2*math.pi*r
 
 def area(r):
