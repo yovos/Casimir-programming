9c5b45e0f6f11269e4049e4b61379d7fc7dded41
Ludwig <hoffmann@lorentz.leidenuniv.nl>
2021-01-27 09:49:46 +0100

    Statistics for all books added to dataframe and RankvFrequency diagram for all books

--- stats.py
+++ stats.py
@@ -9,6 +9,7 @@ import numpy as np
 #nltk.download('stopwords')
 import ast
 import pandas as pd
+import os.path
 
 #######################################################################################################
 #Get filename from entered title
@@ -36,6 +37,13 @@ def get_list_words_from_title():
     else:
         print("Title was not found.")
         exit()
+    data = file.read()
+    data_wout_symbols = re.sub(r'[^\w]', ' ', data) #remove symbols
+    words = data_wout_symbols.split() #split text at space
+    return data_wout_symbols, words
+
+def get_list_words_from_filename(filename):
+    file = open(filename, "r")
     data = file.read()
     data_wout_symbols = re.sub(r'[^\w]', ' ', data) #remove symbols
     words = data_wout_symbols.split() #split text at space
@@ -71,7 +79,7 @@ def Rank_Frequency_Plot(N):
     Frequency_common_words_decreasing = collections.OrderedDict(Frequency_common_words) #Make ordered dictionary of most common words and with which percentage they occur
     #print(frequency_percentage[:5]) #print the five most common words with percentage 
 
-    fig, axes = plt.subplots()
+    #fig, axes = plt.subplots()
     axes.set_xscale('log')
     axes.set_yscale('log')
     axes.set_xlabel(r'Rank', size=15)
@@ -79,7 +87,7 @@ def Rank_Frequency_Plot(N):
     axes.plot(np.arange(0,N,1), Frequency_common_words_decreasing.values())
     #list(frequency_percentage_decreasing).index('is') #Position of a given word in list of most common words
     #plt.bar(frequency_percentage_decreasing.keys(), frequency_percentage_decreasing.values())
-    plt.savefig('diagram.png')
+    #!!!plt.savefig('diagram.png')
     
 #!!!Rank_Frequency_Plot(3000)
 #Rank_Frequency_Plot(len(Count_of_words))
@@ -87,8 +95,43 @@ def Rank_Frequency_Plot(N):
 #print('Number of sentences:', len(sentences))
 
 
+#######################################################################################################
+#Add statistics to all books in dataframe
+#######################################################################################################
+
+fig, axes = plt.subplots()
 Books_dataframe = pd.read_csv("Books_Dataframe.csv") 
 #print(Books_dataframe)
+list_number_of_words = []
+#most_common_letters = []
+most_common_words = []
+most_diverse = []
 for i in range(100):
-    print(Books_dataframe.iloc[i,5])
-#Add all the numbers to the existing dataframe by first storing them in lists and then adding the lists as new columns to dataframe
+    #print(Books_dataframe.iloc[i,5])
+    filename = Books_dataframe.iloc[i,5]
+    #filename = '/' + filename.lstrip('./') #remove leading dot in filename
+    if os.path.exists(filename):
+        data_wout_symbols, words = get_list_words_from_filename(filename)
+        for j in range(len(words)):
+            #make all words lowercase
+            words[j] = words[j].lower()
+        Count_of_letter = collections.Counter(data_wout_symbols) #count how often each letter occurs
+        Count_of_words = collections.Counter(words) #count how often each word occurs
+        list_number_of_words.append(len(words))
+        #most_common_letters.append(Count_of_letter.most_common(2))
+        most_common_words.append(list(collections.OrderedDict(Count_of_words.most_common(2)).keys()))
+        most_diverse.append(len(Count_of_words)/len(words))
+        #Rank_Frequency_Plot(1000)
+    else:
+        list_number_of_words.append("File not found")
+        most_common_words.append("File not found")
+        #most_common_letters.append("File not found")
+        most_diverse.append(0)
+Books_dataframe['Total Number Words'] = list_number_of_words
+#Books_dataframe['Two Most Common Letters'] = most_common_letters
+Books_dataframe['Two Most Common Words'] = most_common_words
+Books_dataframe['Ratio of Unique and Total Words'] = most_diverse
+Books_dataframe.to_csv(r'Books_w_stats.csv', index = False)
+max_index = pd.to_numeric(Books_dataframe['Ratio of Unique and Total Words']).idxmax()
+print(Books_dataframe.iloc[max_index,1])
+#plt.savefig('diagram.png')
